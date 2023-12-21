b9f6a5a48403c1a206ca227655b5d9668af1a5dd
luv nft <info@blkluv.org>
2023-12-21 14:04:05 -0500

    updated

--- src/app/components/Swap.js
+++ src/app/components/Swap.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Swap(){
     return (<><div className="max-w-sm mx-auto my-10 bg-white rounded-lg shadow-md overflow-hidden">
     <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 text-gray-700">兑换</div>
+        <div className="font-bold text-xl mb-2 text-gray-700">Exchange</div>
         <div className="flex justify-between items-center mb-4">
-            <div className="text-gray-700 text-sm">你付钱</div>
+            <div className="text-gray-700 text-sm">Pay</div>
             <div className="flex items-center">
             <Image src="/polkadot.png" className="block appearance-none mx-2  bg-white" alt="usdc logo" width={24} height={24}/>
                 <select id="currency-select"
@@ -20,7 +20,7 @@ export default function Swap(){
         <div className="flex justify-center items-center mb-4">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-arrow-down"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>
         </div>
-        <div className="flex justify-between items-center mb-4"><div className="text-gray-700 text-sm mb-4">你收到</div>
+        <div className="flex justify-between items-center mb-4"><div className="text-gray-700 text-sm mb-4">Receive</div>
         <div className="flex items-center">
                         <Image src="/usdc.png" className="block appearance-none mx-2  bg-white" alt="usdc logo" width={24} height={24}/>
                         <select id="currency-select"
@@ -36,7 +36,7 @@ export default function Swap(){
     </div>
     <div className="px-6 pt-4 pb-2">
     <button
-            className="w-full bg-pink-500 text-white py-2 rounded hover:bg-pink-600 focus:outline-none mb-4">确认交易</button>
+            className="w-full bg-pink-500 text-white py-2 rounded hover:bg-pink-600 focus:outline-none mb-4">Confirm</button>
     </div>
 </div></>)
 }
